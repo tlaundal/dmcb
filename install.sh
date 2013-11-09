4b8c92fcade6b63fb0b02602ffb900d652f63fc1
totokaka <ttotokaka@gmail.com>
2013-11-09 14:48:21 +0000

    moved from install script to requirements.txt

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pip install Pillow
-pip install Flask
-pip install dnspython3
-git clone https://github.com/thadeusb/flask-cache.git
-cd flask-cache
-python setup.py install
-cd ..
-rm -r flask-cache
