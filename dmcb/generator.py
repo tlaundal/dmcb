1118d2ddfd4a5f0d927c33ae4d676aecfaafcae9
totokaka <mail@totokaka.io>
2014-12-05 17:55:35 +0100

    Some small fixes'

--- dmcb/generator.py
+++ dmcb/generator.py
@@ -9,8 +9,9 @@ from dmcb import font, network, get_path
 
 # Parse the background texture
 texture = Image.open(get_path('static/texture.png'))
-texture = texture.resize((60,60))
+texture = texture.resize((60, 60))
 texture = texture.point(lambda p: p * 0.20)
+
 
 def _repeat(image, pattern):
     ''' Repeat the pattern over the image
@@ -26,20 +27,22 @@ def _repeat(image, pattern):
         x += pw
     return image
 
+
 def banner(name, adress, port=25565):
-    
     # Get all info
     server_name = font.parse(name)
-    server_adress = font.parse('§8' + adress)
     if port != 25565:
         server_adress = font.parse('§8' + adress + '§7:§8' + str(port))
+    else:
+        server_adress = font.parse('§8' + adress)
 
     try:
         info = network.get_server_info(adress, port=port)
-        
+
         motd = info['description']
         if type(motd) == dict:
             motd = motd['text']
+        motd = "§7" + motd
         if '\n' in motd:
             motd = motd.split('\n')
         else:
@@ -48,8 +51,8 @@ def banner(name, adress, port=25565):
             else:
                 motd = [motd]
 
-        players = font.parse('§7' + str(info['players']['online'])
-                           + '§8/§7' + str(info['players']['max']))
+        players = font.parse('§7' + str(info['players']['online']) +
+                             '§8/§7' + str(info['players']['max']))
 
         ping = parse_ping(info['ping'])
         icon = info['favicon']
@@ -57,7 +60,7 @@ def banner(name, adress, port=25565):
             icon = Image.open(icon).convert('RGBA')
             icon.load()
 
-    except (error, herror, gaierror, timeout, AssertionError) as ex:
+    except (error, herror, gaierror, timeout, AssertionError):
         motd = ["§4Can't reach server"]
         players = []
         icon = None
@@ -76,20 +79,22 @@ def banner(name, adress, port=25565):
     image = Image.new('RGB', (width, heigth))
     _repeat(image, texture)
     drawer = ImageDraw.Draw(image)
-    
-    y_offset = int(margin + (icon_size - text_size*3)/2) # 7
+
+    y_offset = int(margin + (icon_size - text_size*3)/2)
     font.render((text_offset, y_offset), server_name, image)
-    font.render((text_offset, y_offset + text_size), font.parse(motd[0]), image)
+    font.render((text_offset, y_offset + text_size),
+                font.parse(motd[0]), image)
     if len(motd) > 1:
-        font.render((text_offset, y_offset + text_size*2), font.parse(motd[1]), image)
+        font.render((text_offset, y_offset + text_size*2),
+                    font.parse(motd[1]), image)
 
     adress_width = font.get_width(server_adress)
-    font.render_small((width-margin-adress_width, 63), server_adress, image)
+    font.render_small((width-margin-adress_width, 60), server_adress, image)
 
     if icon is not None:
-        image.paste(icon, (5,5), mask=icon)
-    
-    render_ping(drawer, (width-margin-20,margin), ping)
+        image.paste(icon, (5, 5), mask=icon)
+
+    render_ping(drawer, (width-margin - 20, margin), ping)
 
     players_width = font.get_width(players)
     font.render((width-margin-20-margin-players_width, margin), players, image)
@@ -101,51 +106,68 @@ def banner(name, adress, port=25565):
     mem_file.seek(0)
     return mem_file
 
+
 def render_ping(drawer, xy, ping):
     ''' Render the ping to the supplied Drawer object
     '''
     x, y = xy
-    black_foreground = (91,91,91)
-    black_background = (56,56,56)
-    green_foreground = (0,255,33)
-    green_background = (0,135,15)
+    black_foreground = (91, 91, 91)
+    black_background = (56, 56, 56)
+    green_foreground = (0, 255, 33)
+    green_background = (0, 135, 15)
     if (ping == 1):
-        fills = [green_background,green_foreground,black_background,black_foreground,black_background,black_foreground,black_background,black_foreground,black_background,black_foreground]
+        fills = [green_background, green_foreground, black_background,
+                 black_foreground, black_background, black_foreground,
+                 black_background, black_foreground, black_background,
+                 black_foreground]
     elif (ping == 2):
-        fills = [green_background,green_foreground,green_background,green_foreground,black_background,black_foreground,black_background,black_foreground,
-                 black_background,black_foreground]
+        fills = [green_background, green_foreground, green_background,
+                 green_foreground, black_background, black_foreground,
+                 black_background, black_foreground, black_background,
+                 black_foreground]
     elif (ping == 3):
-        fills = [green_background,green_foreground,green_background,green_foreground,green_background,
-                 green_foreground,black_background,black_foreground,black_background,black_foreground]
+        fills = [green_background, green_foreground, green_background,
+                 green_foreground, green_background, green_foreground,
+                 black_background, black_foreground, black_background,
+                 black_foreground]
     elif (ping == 4):
-        fills = [green_background,green_foreground,green_background,green_foreground,green_background,
-                 green_foreground,green_background,green_foreground,black_background,black_foreground]
+        fills = [green_background, green_foreground, green_background,
+                 green_foreground, green_background, green_foreground,
+                 green_background, green_foreground, black_background,
+                 black_foreground]
     elif (ping == 5):
-        fills = [green_background,green_foreground,green_background,green_foreground,green_background,
-                 green_foreground,green_background,green_foreground,green_background,green_foreground]
+        fills = [green_background, green_foreground, green_background,
+                 green_foreground, green_background, green_foreground,
+                 green_background, green_foreground, green_background,
+                 green_foreground]
     else:
-        fills = [black_background,black_foreground,black_background,black_foreground,black_background,black_foreground,black_background,black_foreground,black_background,black_foreground]
+        fills = [black_background, black_foreground, black_background,
+                 black_foreground, black_background, black_foreground,
+                 black_background, black_foreground, black_background,
+                 black_foreground]
 
-    drawer.rectangle([(x+1*2, y+5*2), (x+ 2*2-1, y+7*2-1)], fill=fills[0])
-    drawer.rectangle([(x+0*2, y+4*2), (x+ 1*2-1, y+6*2-1)], fill=fills[1])
-    drawer.rectangle([(x+3*2, y+4*2), (x+ 4*2-1, y+7*2-1)], fill=fills[2])
-    drawer.rectangle([(x+2*2, y+3*2), (x+ 3*2-1, y+6*2-1)], fill=fills[3])
-    drawer.rectangle([(x+5*2, y+3*2), (x+ 6*2-1, y+7*2-1)], fill=fills[4])
-    drawer.rectangle([(x+4*2, y+2*2), (x+ 5*2-1, y+6*2-1)], fill=fills[5])
-    drawer.rectangle([(x+7*2, y+2*2), (x+ 8*2-1, y+7*2-1)], fill=fills[6])
-    drawer.rectangle([(x+6*2, y+1*2), (x+ 7*2-1, y+6*2-1)], fill=fills[7])
-    drawer.rectangle([(x+9*2, y+1*2), (x+10*2-1, y+7*2-1)], fill=fills[8])
-    drawer.rectangle([(x+8*2, y+0*2), (x+ 9*2-1, y+6*2-1)], fill=fills[9])
+    drawer.rectangle([(x+1*2, y+5*2), (x + 2*2-1, y+7*2-1)], fill=fills[0])
+    drawer.rectangle([(x+0*2, y+4*2), (x + 1*2-1, y+6*2-1)], fill=fills[1])
+    drawer.rectangle([(x+3*2, y+4*2), (x + 4*2-1, y+7*2-1)], fill=fills[2])
+    drawer.rectangle([(x+2*2, y+3*2), (x + 3*2-1, y+6*2-1)], fill=fills[3])
+    drawer.rectangle([(x+5*2, y+3*2), (x + 6*2-1, y+7*2-1)], fill=fills[4])
+    drawer.rectangle([(x+4*2, y+2*2), (x + 5*2-1, y+6*2-1)], fill=fills[5])
+    drawer.rectangle([(x+7*2, y+2*2), (x + 8*2-1, y+7*2-1)], fill=fills[6])
+    drawer.rectangle([(x+6*2, y+1*2), (x + 7*2-1, y+6*2-1)], fill=fills[7])
+    drawer.rectangle([(x+9*2, y+1*2), (x + 10*2-1, y+7*2-1)], fill=fills[8])
+    drawer.rectangle([(x+8*2, y+0*2), (x + 9*2-1, y+6*2-1)], fill=fills[9])
 
     if(ping == -1):
-        drawer.line([(x+4, y), (x+10*2-5, y+6*2-1)], fill=(170,0,0), width=1)
-        drawer.line([(x+3, y), (x+10*2-6, y+6*2-1)], fill=(170,0,0), width=1)
-        drawer.line([(x+4, y+6*2-1), (x+10*2-5, y)], fill=(170,0,0), width=1)
-        drawer.line([(x+5, y+6*2-1), (x+10*2-4, y)], fill=(170,0,0), width=1)
+        fill = (170, 0, 0)
+        drawer.line([(x+4, y), (x+10*2-5, y+6*2-1)], fill=fill, width=1)
+        drawer.line([(x+3, y), (x+10*2-6, y+6*2-1)], fill=fill, width=1)
+        drawer.line([(x+4, y+6*2-1), (x+10*2-5, y)], fill=fill, width=1)
+        drawer.line([(x+5, y+6*2-1), (x+10*2-4, y)], fill=fill, width=1)
 
     # Return the size
     return (9*2-1, 6*2-1)
-    
+
+
 def parse_ping(ping):
     ''' Parse a ping in ms to a format readable by render_ping
     '''
@@ -159,5 +181,4 @@ def parse_ping(ping):
         return 2
     elif ping > 0:
         return 1
-    else:
-        return -1   
+    return -1
